3438b6602dc5320bd907edae6cdfd0672ff3ee5d
RinkalBhimani <rinkalbhimani24@gmail.com>
2024-10-16 10:53:26 +0530

    Update emailapi.php

--- emailapi.php
+++ emailapi.php
@@ -29,8 +29,8 @@ $mail = new PHPMailer(true);
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'rinkalbhimani24@gmail.com';                     //SMTP username
-    $mail->Password   = 'xwyl vlla crem atpy';                               //SMTP password
+    $mail->Username   = 'your username';                     //SMTP username
+    $mail->Password   = 'your passkey';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;       
 
